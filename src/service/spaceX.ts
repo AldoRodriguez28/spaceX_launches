8c7db76d9abd1cf9fdd2fa20be46093090afb3ad
AldoRodriguez28 <arodriguez.ronquillo@hotmail.com>
2024-10-10 10:51:33 -0600

    actualizanod archivos de deploy

---
+++ src/service/spaceX.ts
@@ -0,0 +1,59 @@
+
+import {type Doc, type APISpaceXResponose } from "../types/api";
+
+export const getLauncheById = async ({id}: {id:string}) =>{
+ 
+    const response = await fetch(`https://api.spacexdata.com/v5/launches/${id}`);  
+
+        const launches = (await response.json()) as Doc
+
+return launches;
+}
+
+export const getLantestLaunches = async () =>{
+ 
+        const response = await fetch('https://api.spacexdata.com/v5/launches/query',{
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            options: {
+                sort:{
+                    date_unix:'asc'
+                },
+                limit: 12,
+            }
+        })
+    });  
+        if (!response.ok) {
+                throw new Error(`Error: ${response.status} - ${response.statusText}`);
+            }
+            const {docs: launches} = (await response.json()) as APISpaceXResponose
+   
+    return launches;
+}
+
+export const getOldestLaunches = async () =>{
+ 
+    const response = await fetch('https://api.spacexdata.com/v5/launches/query',{
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        options: {
+            sort:{
+                date_unix:'desc'
+            },
+            limit: 12,
+        }
+    })
+});  
+    if (!response.ok) {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        }
+        const {docs: launches} = (await response.json()) as APISpaceXResponose
+
+return launches;
+}
